89bea015327cdfed3a1fb6d170c87b0ea8973086
Yibo Liu <buaalyb@buaa.edu.cn>
2020-02-21 14:32:46 -0500

    QRcode

---
+++ build/CMakeFiles/QRCODE.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QRCODE.dir/QRCODE.cpp.o"
+  "QRCODE.pdb"
+  "QRCODE"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QRCODE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
